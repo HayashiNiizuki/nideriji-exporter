889fcbb34159d1b595e10313001559bc5e957192
Liu Ping <hayashi_niizuki@outlook.com>
2025-08-13 10:59:54 +0800

    exception handling

--- main.py
+++ main.py
@@ -50,27 +50,33 @@ def save_diaries():
 
     diaries = res.json()["diaries"]
     for diary in diaries:
-        _id = diary["id"]
-        payload = {"diary_ids": _id}
-        res = requests.post(
-            url=BASE_URL + "diary/all_by_ids/" + str(USER_ID) + "/",
-            data=payload,
-            headers=headers,
-            timeout=10,
-        )
-        diaries = res.json().get("diaries", [])
-        if diaries:
-            diary = diaries[0]
-            dt_str = datetime.fromtimestamp(diary["createdtime"]).strftime("%Y-%m-%d")
-            title = diary["title"]
-            with open(
-                f"./.data/{dt_str}-{_id}-{title}.txt", "w", encoding="utf-8"
-            ) as f:
-                f.write(diary["title"] + "\n\n")
-                f.write(diary["content"])
-                LOG.info(f"Saved diary ID: {_id}")
-        else:
-            LOG.info(f"No diary found for ID: {_id}")
+        try:
+            _id = diary["id"]
+            payload = {"diary_ids": _id}
+            res = requests.post(
+                url=BASE_URL + "diary/all_by_ids/" + str(USER_ID) + "/",
+                data=payload,
+                headers=headers,
+                timeout=10,
+            )
+            diaries = res.json().get("diaries", [])
+            if diaries:
+                diary = diaries[0]
+                dt_str = datetime.fromtimestamp(diary["createdtime"]).strftime(
+                    "%Y-%m-%d"
+                )
+                title = diary["title"]
+                with open(
+                    f"./.data/{dt_str}-{_id}-{title}.txt", "w", encoding="utf-8"
+                ) as f:
+                    f.write(diary["title"] + "\n\n")
+                    f.write(diary["content"])
+                    LOG.info(f"Saved diary ID: {_id}")
+            else:
+                LOG.info(f"No diary found for ID: {_id}")
+        except Exception as e:
+            LOG.error(f"Error saving diary ID {_id}: {e}")
+            continue
 
 
 if __name__ == "__main__":
@@ -79,5 +85,5 @@ if __name__ == "__main__":
     login()
     LOG.info(f"Logged in as user ID: {USER_ID}")
 
-    if args.save_diaries: 
+    if args.save_diaries:
         save_diaries()
